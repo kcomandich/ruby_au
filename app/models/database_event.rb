044a6072d8e2e6ebdc46cc631743a37a0d55e0e2
Kirsten Comandich <kcomandich@gmail.com>
2026-07-17 13:26:06 -0700

    Generate the slug on DatabaseEvent creation using FriendlyId

--- app/models/database_event.rb
+++ app/models/database_event.rb
@@ -23,6 +23,9 @@
 class DatabaseEvent < ApplicationRecord
   self.table_name = "events"
 
+  extend FriendlyId
+  friendly_id :date_region_event_type, use: :slugged
+
   has_many   :talks, dependent: :destroy, inverse_of: :event
   belongs_to :venue
 
@@ -48,5 +51,14 @@ class DatabaseEvent < ApplicationRecord
 
   def self.past(amount = nil)
     before_today.limit(amount)
+  end
+
+  def date_region_event_type
+    formatted_date = date.strftime("%Y-%m-%d")
+    "#{formatted_date}-ruby-#{region}-#{event_type}"
+  end
+
+  def should_generate_new_friendly_id?
+    date_changed? || region_changed? || event_type_changed? || super
   end
 end
