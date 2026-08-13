598bd578585aeecc8332b1003a482bb2487c9738
Kirsten Comandich <kcomandich@gmail.com>
2026-08-13 12:50:52 -0700

    minor: include venue when fetching events on the Melbourne home page

--- sites/melbourne/app/controllers/database_events_home_controller.rb
+++ sites/melbourne/app/controllers/database_events_home_controller.rb
@@ -3,7 +3,7 @@
 module Melbourne
   class DatabaseEventsHomeController < ApplicationController
     def show
-      @next_event = DatabaseEvent.upcoming(1).first
+      @next_event = DatabaseEvent.upcoming(1).includes(:venue).first
       @past_events = DatabaseEvent.past(4)
     end
   end
